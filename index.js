29de602825441f90d72e583eef364f7e35a23640
Lamine-br <jl_brahami@esi.dz>
2023-12-28 23:43:37 +0100

    each navbar item is related to a page/ page contact initialized

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ elements.forEach((element) => {
 		element.classList.add("active");
 	});
 });
+
+const listItems = document.querySelectorAll("li");
+
+const sections = document.querySelectorAll(".section");
+
+listItems.forEach((li) => {
+	li.addEventListener("click", () => {
+		const sectionId = li.getAttribute("data-section");
+
+		sections.forEach((section) => {
+			section.classList.remove("active2");
+		});
+
+		const selectedSection = document.getElementById(sectionId);
+		if (selectedSection) {
+			selectedSection.classList.add("active2");
+		}
+	});
+});
